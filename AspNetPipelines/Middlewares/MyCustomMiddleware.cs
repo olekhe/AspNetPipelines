9cdbea7ee26506cd2e47c8db3e9572f4afeec9d0
Andrii Nasheba <a.nasheba@xtoniq.com>
2023-03-10 20:27:07 +0200

    Task 3: added a check for all requests that do not contain the "Referred-From: some-cool-app.com" header

--- AspNetPipelines/Middlewares/MyCustomMiddleware.cs
+++ AspNetPipelines/Middlewares/MyCustomMiddleware.cs
@@ -15,7 +15,14 @@ namespace AspNetPipelines.Middlewares
         [HttpGet]
         public async Task InvokeAsync(HttpContext context)
         {
+            context.Response.Headers.Add("Reffered-From", "some-cool-app.com");
             Debug.WriteLine("-- MyCustomMiddleware before --");
+            if (!context.Request.Headers.ContainsKey("Reffered-From") ||
+                !context.Request.Headers["Reffered-From"].ToString().Contains("some-cool-app.com"))
+            {
+                context.Response.StatusCode = 403; // Forbidden
+                return;
+            }
 
             await this.next(context);
 
