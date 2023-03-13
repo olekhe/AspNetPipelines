bcb7a851e0209abf02d61c1d7c506863ee2fa2d2
Oleksandr Herhelezhyu <116067125+olekhe@users.noreply.github.com>
2023-03-13 13:11:14 +0100

    Update WeatherForecastController.cs

--- AspNetPipelines/Controllers/WeatherForecastController.cs
+++ AspNetPipelines/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace AspNetPipelines.Controllers
 {
@@ -9,8 +10,16 @@ namespace AspNetPipelines.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
+        // енаму в контроллері не місце
+        public enum Temper
+        {
+            Celsius,
+            Fahrenheit
+        }
+
+        // приватні рідонлі філди в одному місці мають бути
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -19,15 +28,25 @@ namespace AspNetPipelines.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> GetWeatherForecast()
+        // параметрів стає багато. час об'єднати в якесь Dto
+        public IEnumerable<WeatherForecast> GetWeatherForecast(int days = 3, string city = "Chernivtsi", Temper temper = Temper.Celsius)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(0, days).Select(index => new WeatherForecast
             {
+                // просто повертаєш місто?
+                // не відповідає поставленому завданню. Дивися завдання.
+                City = city,
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
+                //TemperatureC = Random.Shared.Next(-20, 55),
+                Temperature = new Dictionary<string, int> { { temper.ToString(), temper == Temper.Celsius ? 
+                                                            // в окрему функцію перетворення градусів
+                                                            Random.Shared.Next(-20, 55) : 
+                                                            (Random.Shared.Next(-20, 55) * 9 / 5 + 32) } },
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
+
+        // невистачає функціоналу для додавання понгодних даних
     }
 }
